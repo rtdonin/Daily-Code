3ae84f03cdb6f0ced22297f4980f32f663c5d3e9
Margaret Donin <rtdonin@gmail.com>
2022-10-27 11:31:29 -0400

    found a better a simpler solution

--- canMakeArithmeticProgression.java
+++ canMakeArithmeticProgression.java
@@ -50,3 +50,23 @@ class Solution {
         return true;
     }
 }
+
+// The quicker solution is just to sort the array.
+// I was wondering if that was the case.
+// This makes perfect sense.
+
+class Solution {
+    public boolean canMakeArithmeticProgression(int[] arr) {
+        
+        Arrays.sort(arr);
+        int d = Math.abs(arr[1] - arr[0]);
+
+        for(int i = 1; i< arr.length; i++){
+            if(Math.abs(arr[i] - arr[i - 1]) != d) {
+                return false;
+            }
+        }
+        return true;
+        
+    }
+}
